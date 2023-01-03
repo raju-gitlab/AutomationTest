7d55cf796cf724584be29bdb52d442c7573c76c9
Raju.b@Basak <63355575+raju-gitlab@users.noreply.github.com>
2023-01-03 22:27:56 +0530

    Add project files.

---
+++ AutomationTest/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AutomationTest.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public bool Can_view_page(string usertype)
+        {
+            if (usertype.ToLower() == "admin")
+            {
+                return true;
+            }
+            else if(usertype.ToLower() == "user")
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,49 @@
+﻿using AutomationTest.Controllers;
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NUnit.Framework;
+using Assert = NUnit.Framework.Assert;
+
+
+namespace UnitTestProject1
+{
+    [TestFixture]
+    public class UnitTest1
+    {
+        [Test]
+        public void can_userauth_as_admin()
+        {
+            var test = new HomeController();
+            var result = test.Can_view_page("admin");
+            Assert.That(result, Is.EqualTo(true));
+        }
+
+    }
+
+    [TestClass]
+    public class UnitTest2
+    {
+        [TestMethod]
+        public void can_userauth_as_user()
+        {
+            var test = new HomeController();
+            var result = test.Can_view_page("USER");
+            Assert.That(result, Is.EqualTo(true));
+        }
+
+    }
+
+    [TestClass]
+    public class UnitTest3
+    {
+        [TestMethod]
+        public void can_userauth_as_other()
+        {
+            var test = new HomeController();
+            var result = test.Can_view_page("");
+            Assert.That(result, Is.EqualTo(false));
+        }
+
+    }
+
+}
